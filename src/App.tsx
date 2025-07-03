2b18cc0979b6ba11de85ded6f764ae0b05e9b168
Bloomquist <dbloomquist@AdvantageFutures.com>
2025-07-03 13:37:19 -0500

    more progress

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,17 @@ function Square({ value, onSquareClick }: SquareProps) {
 	);
 }
 
-export default function Board() {
+function Board() {
    const [xNext, setXNext] = useState(true);
    const [squares, setSquares] = useState(Array(9).fill(null));
+   const winner = calculateWinner(squares);
+
+   let status;
+   if (winner)	{ status = "Winner: " + winner + "!"; }
+   else 		{ status = "Next player: " + (xNext ? "X" : "O"); }
 
    function handleClick(i: number) {
-	if(squares[i]){ return; }
+	if(squares[i] || calculateWinner(squares)){ return; }
 	const nextSquares = squares.slice();
 	if(xNext)	{ nextSquares[i] = "X"; }
 	else 		{ nextSquares[i] = "O"; }
@@ -26,6 +31,7 @@ export default function Board() {
 
    return (
 	<>
+		<div className="status">{status}</div>
 		<div className="board-row">
 			<Square value={squares[0]} onSquareClick={() => handleClick(0)} />
 			<Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -42,5 +48,27 @@ export default function Board() {
 			<Square value={squares[8]} onSquareClick={() => handleClick(8)} />
 	   </div>
 	</>
+	);
+}
+
+function calculateWinner(squares) {
+	const lines = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ];
+	for(let i=0; i < lines.length; i++) {
+		const [a,b,c] = lines[i];
+		if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+			return squares[a];
+		}
+	}
+	return null;
+}
+
+export default function Game() {
+	return (
+		<div className="game">
+			<div className="game-board"><Board /></div>
+			<div className="game-info">
+				<ol>{/* TODO */}</ol>
+			</div>
+		</div>
 	);
 }
